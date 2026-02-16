379d28f0bc84ecfc1fcf5d266b643cd11458001d
Billy5691-2 <Billymortimer1965@gmail.com>
2026-02-16 20:58:03 +0000

    chore: Rename HelperFunctions namespace to Common
    Rename the helper function namespace to common.
    Also apply clang format rules to the file.

--- src/common/helper_functions.hpp
+++ src/common/helper_functions.hpp
@@ -1,36 +1,31 @@
 #ifndef B1D235EC_A2A8_4785_AEEC_044AE6A82F53
 #define B1D235EC_A2A8_4785_AEEC_044AE6A82F53
-#include <vector>
 #include <iostream>
+#include <vector>
 
-namespace HelperFunctions {
-  template <typename t>
-  void PrintVector(const std::vector<t>& vec, int newLineInterval)
-  {
-    for (int i = 0; i < vec.size; i++)
-    {
-      std::cout << vec[i];
-      if (i % newLineInterval == 0)
-      {
-        std::cout << "\n";
-      }
+namespace Common {
+    template <typename t>
+    void PrintVector(const std::vector<t>& vec, int newLineInterval) {
+        for (int i = 0; i < vec.size; i++) {
+            std::cout << vec[i];
+            if (i % newLineInterval == 0) {
+                std::cout << "\n";
+            }
+        }
     }
-  }
 
-  template <typename t>
-  void VectorAppendToStart(const std::vector<t>& dataVec, std::vector<t>* targetVec)
-  {
-    targetVec->reserve(targetVec->size() + dataVec.size());
-    targetVec->insert(targetVec->begin(), dataVec.begin(), dataVec.end());
-  }
+    template <typename t>
+    void VectorAppendToStart(const std::vector<t>& dataVec, std::vector<t>* targetVec) {
+        targetVec->reserve(targetVec->size() + dataVec.size());
+        targetVec->insert(targetVec->begin(), dataVec.begin(), dataVec.end());
+    }
 
-  template <typename t>
-  void VectorAppendToEnd(const std::vector<t>& dataVec, std::vector<t>* targetVec)
-  {
-    targetVec->reserve(targetVec->size() + dataVec.size());
-    targetVec->insert(targetVec->end(), dataVec.begin(), dataVec.end());
-  }
-}  // namespace HelperFunctions
+    template <typename t>
+    void VectorAppendToEnd(const std::vector<t>& dataVec, std::vector<t>* targetVec) {
+        targetVec->reserve(targetVec->size() + dataVec.size());
+        targetVec->insert(targetVec->end(), dataVec.begin(), dataVec.end());
+    }
+} // namespace Common
 
 
 #endif /* B1D235EC_A2A8_4785_AEEC_044AE6A82F53 */
